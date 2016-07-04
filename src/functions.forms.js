f33bbc2e5299ed67fc61ce476e733a79d2182338
Franpastoragusti <fran.sportmaniacs@gmail.com>
2016-07-04 14:30:09 +0200

    Name, type, dependent, required acabados

--- src/functions.forms.js
+++ src/functions.forms.js
@@ -37,19 +37,57 @@ module.exports = new function() {
 
         hashMap.name = field.name;
 
+        // CONDICIONES RESPECTO AL TYPE
         if (field.type)
             hashMap["type"] = field.type;
 
-        if (field.type == "select")
+        if (field.type == "select" || field.type == "product")//product ¿es un tipo válido?
             {
                 elementType = "select";
                 delete hashMap.type;
-                
             }
 
+        if (field.type == "select" && field.label == "Club")
+        {
+            elementType = "input";
+            hashMap["type"] = "text";
+        }
 
+        if (field.type == "phone" || field.type == "number")//¿esto tiene que camiar los tipos de verdad a text?
+            hashMap["type"] = "text";
+
+        if (field.label == "Prefijo") // ni idea de como validar esto
+        {
+            delete hashMap.type;
+            delete hashMap.name;
+        }
+        if (field.label == "Género")// no sabemos como diferenciar entre un select de verdad y un radio
+            elementType = "input";
+
+        if (field.type == "date")
+            delete hashMap.type;
+
+        //FIN DE LAS CONDICIONES RESPECTO AL TYPE
+
+
+        //CONDICIONES RESPECTO A REQUIRED
+        if (field.required == true)
+            hashMap["required"] = 'required';
+
+        if (field.type == "select")//Establece el required en el label
+            delete hashMap["required"];
+
+        //FIN DE LAS CONDICIONES RESPECTO AL REQUIRED
+        
+
+
+        //CONDICIONES RESPECTO A DEPENDENT
         if(field.dependent)
             hashMap["data-dependent"] = JSON.stringify(field.dependent);
+
+        //FIN DE LAS CONDICIONES RESPECTO AL DEPENDENT
+
+
 
 
         for (var attribute in hashMap)
