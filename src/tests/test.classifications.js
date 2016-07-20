1d638c140acd57964c149e124a46cb9798756152
emilioheras <emirobor@gmail.com>
2016-07-20 09:52:15 +0200

    subida del archivo test de clasificaciones.

---
+++ src/tests/test.classifications.js
@@ -0,0 +1,23 @@
+var navegation      = require("../navegation.functions");
+var dataBuilder     = require("../testData.builder");
+var api             = "http://api.local.sportmaniacs.com";
+
+module.exports = {
+
+	"Test Clasificaciones" : function (browser) {
+
+		var races = dataBuilder.buildTestData(api);
+
+        navegation.login(browser, "user+test00@gmail.com", "123456");
+
+        // navegation.goToEventClassificationPage(browser, "97", "330");
+
+        races.forEach(function(race) {
+            race.events.forEach(function(event) {
+                navegation.goToEventClassificationPage(browser, race.id, event.id);
+              
+            });
+        });
+	}
+
+};
