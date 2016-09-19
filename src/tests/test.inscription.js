4048f347aaf9cc66568b01fadb370c005a09807f
Franpastoragusti <franciscopa91@gmail.com>
2016-09-19 16:57:30 +0200

    Test sin pago

--- src/tests/test.inscription.js
+++ src/tests/test.inscription.js
@@ -12,41 +12,42 @@ var api             = "http://api.local.sportmaniacs.com";
 
 module.exports = {
 
-    // "Test Inscriptions" : function (browser) {
-    //     navegation.login(browser, "alberto@sportmaniacs.com", "123456");
-    //     var races   = dataBuilder.buildTestData(api);
-    //     races.forEach(function (race) {
-    //         browser.getLog(race.name, function(){
-    //             console.log(race.name);
-    //         });
-    //         race.events.forEach(function (event) {
-    //             if (event.form) {
-    //                 navegation.goToEventPage(browser, race.id, event.id);
-    //                 var user = userBuilder.buildAppropriateUser(event.form.fields, event);
-    //             }
-    //             if(event.form.steps && event.form.steps.length) {
-    //                 event.form.steps = formFunctions.recalculateStepsForTeamInscriptions(event.form.steps, user);
-    //                 event.form.steps.forEach((step, index) => {
-    //                     if(formFunctions.stepIsAnInscription(step))
-    //                         navegation.clickImRegisteringAFriend(browser);
-    //
-    //                     formFunctions.fillStepFields(browser, user);
-    //                     navegation.goToNextStep(browser);
-    //                 });
-    //                 browser.getText("#the-price > tbody > tr:last-child > td:last-child", function (result) {
-    //                     var sumaryPrice = parseFloat(result.value);
-    //                     navegation.clickOnPayButton(browser);
-    //                     if (sumaryPrice > 0) {
-    //                         formFunctions.ImOnTheTPV(browser)
-    //                     }
-    //                     if (sumaryPrice <= 0)
-    //                         formFunctions.ImOnTheThankyouPage(browser)
-    //                 });
-    //             }
-    //         });
-    //     });
-    //     browser.end()
-    // },
+    "Test Inscriptions" : function (browser) {
+        navegation.login(browser, "alberto@sportmaniacs.com", "123456");
+        var races   = dataBuilder.buildTestData(api);
+        races.forEach(function (race) {
+            browser.getLog(race.name, function(){
+                console.log(race.name);
+            });
+            race.events.forEach(function (event) {
+                if (event.form) {
+                    navegation.goToEventPage(browser, race.id, event.id);
+                    var user = userBuilder.buildAppropriateUser(event.form.fields, event);
+                }
+                if(event.form.steps && event.form.steps.length) {
+                    event.form.steps = formFunctions.recalculateStepsForTeamInscriptions(event.form.steps, user);
+                    event.form.steps.forEach((step, index) => {
+                        if(formFunctions.stepIsAnInscription(step))
+                            navegation.clickImRegisteringAFriend(browser);
+
+                        formFunctions.fillStepFields(browser, user);
+                        navegation.goToNextStep(browser);
+                    });
+                    browser.getText("#the-price > tbody > tr:last-child > td:last-child", function (result) {
+                        var sumaryPrice = parseFloat(result.value);
+                        navegation.clickOnPayButton(browser);
+                        if (sumaryPrice > 0) {
+                            //formFunctions.ImOnTheTPV(browser)
+                            formFunctions.ImOnTheTPVWithoutSetPrice(browser)
+                        }
+                        if (sumaryPrice <= 0)
+                            formFunctions.ImOnTheThankyouPage(browser)
+                    });
+                }
+            });
+        });
+        browser.end()
+    },
     "Test Inscriptions with a bad user": function (browser) {
         navegation.login(browser, "alberto@sportmaniacs.com", "123456");
         var races = dataBuilder.buildTestData(api);
@@ -81,7 +82,8 @@ module.exports = {
                         var sumaryPrice = parseFloat(result.value);
                         navegation.clickOnPayButton(browser);
                         if (sumaryPrice > 0) {
-                            formFunctions.ImOnTheTPV(browser)
+                            //formFunctions.ImOnTheTPV(browser)
+                            formFunctions.ImOnTheTPVWithoutSetPrice(browser)
                         }
                         if (sumaryPrice <= 0)
                             formFunctions.ImOnTheThankyouPage(browser)
