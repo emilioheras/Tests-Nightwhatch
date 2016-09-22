ad7f046006552d63cde15a888ff33f5c60b20f34
Franpastoragusti <franciscopa91@gmail.com>
2016-09-22 19:00:44 +0200

    test completo

--- src/tests/test.inscription.js
+++ src/tests/test.inscription.js
@@ -11,43 +11,43 @@ var api             = "http://api.local.sportmaniacs.com";
 
 
 module.exports = {
-//
-//     "Test Inscriptions" : function (browser) {
-//         browser.resizeWindow(1900, 1200);
-//         navegation.login(browser, "alberto@sportmaniacs.com", "123456");
-//         var races   = dataBuilder.buildTestData(api);
-//         races.forEach(function (race) {
-//
-//             race.events.forEach(function (event) {
-//                 if (event.form) {
-//                     navegation.goToEventPage(browser, race.id, event.id);
-//                     var user = userBuilder.buildAppropriateUser(event.form.fields, event);
-//                 }
-//                 if(event.form.steps && event.form.steps.length) {
-//                     browser.verify.urlContains(race.id, "Testing the event " + event.id + " of the race " + race.id);
-//                     event.form.steps = formFunctions.recalculateStepsForTeamInscriptions(event.form.steps, user);
-//                     event.form.steps.forEach((step, index) => {
-//                         if (formFunctions.stepIsAnInscription(step)) {
-//                             navegation.clickImRegisteringAFriend(browser);
-//                         }
-//                         formFunctions.fillStepFields(browser, user);
-//                         navegation.goToNextStep(browser);
-//                     });
-//                     browser.getText("#the-price > tbody > tr:last-child > td:last-child", function (result) {
-//                         var sumaryPrice = parseFloat(result.value);
-//                         navegation.clickOnPayButton(browser);
-//                         if (sumaryPrice > 0) {
-//                             //formFunctions.ImOnTheTPV(browser)
-//                             formFunctions.ImOnTheTPVWithoutSetPrice(browser)
-//                         }
-//                         if (sumaryPrice <= 0)
-//                             formFunctions.ImOnTheThankyouPage(browser)
-//                     });
-//                 }
-//             });
-//         });
-//         browser.end()
-//     },
+
+    "Test Inscriptions" : function (browser) {
+        browser.resizeWindow(1900, 1200);
+        navegation.login(browser, "alberto@sportmaniacs.com", "123456");
+        var races   = dataBuilder.buildTestData(api);
+        races.forEach(function (race) {
+
+            race.events.forEach(function (event) {
+                if (event.form) {
+                    navegation.goToEventPage(browser, race.id, event.id);
+                    var user = userBuilder.buildAppropriateUser(event.form.fields, event);
+                }
+                if(event.form.steps && event.form.steps.length) {
+                    browser.verify.urlContains(race.id, "Testing the event " + event.id + " of the race " + race.id);
+                    event.form.steps = formFunctions.recalculateStepsForTeamInscriptions(event.form.steps, user);
+                    event.form.steps.forEach((step, index) => {
+                        if (formFunctions.stepIsAnInscription(step)) {
+                            navegation.clickImRegisteringAFriend(browser);
+                        }
+                        formFunctions.fillStepFields(browser, user);
+                        navegation.goToNextStep(browser);
+                    });
+                    browser.getText("#the-price > tbody > tr:last-child > td:last-child", function (result) {
+                        var sumaryPrice = parseFloat(result.value);
+                        navegation.clickOnPayButton(browser);
+                        if (sumaryPrice > 0) {
+                            //formFunctions.ImOnTheTPV(browser)
+                            formFunctions.ImOnTheTPVWithoutSetPrice(browser)
+                        }
+                        if (sumaryPrice <= 0)
+                            formFunctions.ImOnTheThankyouPage(browser)
+                    });
+                }
+            });
+        });
+        browser.end()
+    },
     "Test Inscriptions with a bad user": function (browser) {
         browser.resizeWindow(1900, 1200);
         navegation.login(browser, "alberto@sportmaniacs.com", "123456");
